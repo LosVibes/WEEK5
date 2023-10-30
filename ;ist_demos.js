0a774c1804475644136a2579504da0db858cd80d
Juan Carlos Hernandez <hernandezjuanjc@gmail.com>
2023-10-30 11:37:00 -0500

    arrays aka lists

---
+++ ;ist_demos.js
@@ -0,0 +1,22 @@
+// ARRY AKA a List/Vector Ordered series of Values
+let pets = ["Bianca","finneley","Dandy"];
+console.log( pets[2]);//DANDY 2 from the beginning
+
+//TWO INITIAL
+let people = [
+    {
+        name: "Carlos",
+        age:29
+    },
+    {
+        name: "Meg",
+        age:30
+    }
+];
+//Add one more 
+people.push    ({name:"Miguel", age:19});
+
+//for(START, STOP, HOW)
+for(let index = 0; index < people.length; index++){
+    console.log(people[index].name, people[index].age);
+}
