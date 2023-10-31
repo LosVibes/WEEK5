395eaeb491aab5e18fb293fcf3f1bee8ed177201
Juan Carlos Hernandez <hernandezjuanjc@gmail.com>
2023-10-31 12:01:30 -0500

    grid

---
+++ grid.js
@@ -0,0 +1,45 @@
+// X . X
+// O . O
+// O X O
+// CHESS or CHECKERS 8X8
+// 19X19 
+
+// *****
+// ****
+// **
+// *
+
+// . . . . . . . .
+// . . . . . . . .
+// . . . . . . . .
+// . . . . . . . .
+// . . . . . . . .
+// . . . . . . . .
+// . . . . . . . .
+// . . . . . . . .
+
+const width = 12 
+const height = 8
+
+for( r = 0; r < height; r++) {
+    let row = "";
+    for(let c = 0; c < width; c++) {
+        row += " .";
+    }
+    console.log("R"+r + row)
+}
+//END of GRID
+const grid = [
+    ['X','O','.'], 
+    ['.','.','.'], 
+    ['.','O','X'], 
+];
+grid[1][1] = "X"
+for(let r = 0; r < 3; r++) {
+    let row = "";
+    for(let c = 0; c < 3; c++) {
+        row += ' ' + grid[r][c];
+    }
+    console.log("R"+r + row)
+}
+
